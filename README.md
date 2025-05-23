7fb189dd1450e4a33a7c38a94fec8a43b503bf83
ben-Lassina <24175@ma-web.nl>
2025-05-23 11:33:39 +0200

    m8progchildtheme uitgewerkt

---
+++ README.md
@@ -0,0 +1,7 @@
+#M8-PROG
+
+Deze repository is opnieuw aangemaakt, omdat in de originele repository per ongeluk gevoelige gegevens waren geüpload. Deze nieuwe versie bevat die gegevens niet.
+
+
+#M8-PROG
+Mijn idee is dat ik de Ma-International website wat uitgebreider maak.
---
+++ themes/ma-international/single.php
@@ -0,0 +1,20 @@
+<?php
+$articles = [
+    [
+        'title' => '1e buitenlandse stage',
+        'content' => '1e ervaring over stage lopen in het buitenland was supertof, dankzij MA-international hebben zij mij geholpen om naar het buitenland een stage te krijgen'
+    ],
+    [
+        'title' => 'Hulp bij internationale stage zoeken',
+        'content' => 'Hoe het Ma mij heeft geholpen om een internationale stage te kunnen pakken'
+    ],
+    [
+        'title' => 'Werken in het buitenland dankzij Ma-International',
+        'content' => 'Na mijn stage via Ma-International, had ik een kans gekregen om in het buitenland te kunnen blijven werken! Wat supertof is!'
+    ],
+      [
+        'title' => 'Hoe zoek je een stage in het buitenland?',
+        'content' => 'Hier zijn een aantal tips om een stage in het buitenland te vinden. Het is belangrijk dat je goed voorbereid bent en weet wat je wilt. MA-International kan je hierbij helpen!'
+    ]
+];
+?>
